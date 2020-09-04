3d86b303f2b573c86ba53b307459c24504fead4c
github username <github email address>
2020-09-04 01:09:11 -0400

    added some bootstrapping and adjusted css, changed numberClick function

--- src/components/EmotionForm.js
+++ src/components/EmotionForm.js
@@ -5,16 +5,17 @@ const EmotionForm = (props) => {
 
         return (
             <div data-name={props.name}>
-                <p>{props.name}: </p>
-                <span onClick={props.numberClick}>1 </span>
-                <span onClick={props.numberClick}>2 </span>
-                <span onClick={props.numberClick}>3 </span>
-                <span onClick={props.numberClick}>4 </span>
-                <span onClick={props.numberClick}>5 </span>
-                <span onClick={props.numberClick}>6 </span>
-                <span onClick={props.numberClick}>7 </span>
-                <span onClick={props.numberClick}>8 </span>
-                <span onClick={props.numberClick}>9 </span>
+                <p className="gradient">{props.name}: </p>
+                <span onClick={props.numberClick}>0 </span> 
+                <span onClick={props.numberClick}>1 </span> 
+                <span onClick={props.numberClick}>2 </span>  
+                <span onClick={props.numberClick}>3 </span>  
+                <span onClick={props.numberClick}>4 </span>  
+                <span onClick={props.numberClick}>5 </span>  
+                <span onClick={props.numberClick}>6 </span>  
+                <span onClick={props.numberClick}>7 </span>  
+                <span onClick={props.numberClick}>8 </span>  
+                <span onClick={props.numberClick}>9 </span>  
                 <span onClick={props.numberClick}>10</span>
             </div>
         );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import EmotionForm from './EmotionForm.js';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 
 class Form extends Component {
@@ -11,7 +15,7 @@ class Form extends Component {
             Anger: '0',
             Sadness: '0',
             Anxiety: '0',
-            Hapiness: '0',
+            Happiness: '0',
             Peacefulness: '0',
             Gratitude: '0',
         };
@@ -19,24 +23,41 @@ class Form extends Component {
 
     numberClick = (e) => {
         e.persist();
-        e.target.className === "chosen-number" ? e.target.className = "" : e.target.className = "chosen-number"
-        this.setState({
-            [e.target.parentElement.dataset.name]: e.target.innerText
-        });
-        console.log(this.state);
+        if (e.target.className === "chosen-number") {
+            e.target.className = ""
+            this.setState({
+                [e.target.parentElement.dataset.name]: '0'
+            });
+        } else {
+            e.target.className = "chosen-number"
+            this.setState({
+                [e.target.parentElement.dataset.name]: e.target.innerText.split(" ")[0]
+            });
         }
+        console.log(this.state);
+    }
 
 
     render() {
         return(
             <div>
-                <EmotionForm name={"Fear"}         numberClick={this.numberClick} />
-                <EmotionForm name={"Anger"}        numberClick={this.numberClick} />
-                <EmotionForm name={"Sadness"}      numberClick={this.numberClick} />
-                <EmotionForm name={"Anxiety"}      numberClick={this.numberClick} />
-                <EmotionForm name={"Happiness"}    numberClick={this.numberClick} />
-                <EmotionForm name={"Peacefulness"} numberClick={this.numberClick} />
-                <EmotionForm name={"Gratitude"}    numberClick={this.numberClick} />
+                <h3>Click on the number correlating to level of intensity of your emotions:</h3>
+                <h4>0 being not at all and 10 being very intense</h4>
+                <Container>
+                    <Row className="justify-content-md-center">
+                        <Col md={3}><EmotionForm name={"Fear"} numberClick={this.numberClick} /></Col>
+                        <Col md={3}><EmotionForm name={"Anger"} numberClick={this.numberClick} /></Col>
+                        <Col md={3}><EmotionForm name={"Sadness"} numberClick={this.numberClick} /></Col>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <Col md={3}><EmotionForm name={"Anxiety"} numberClick={this.numberClick} /></Col>
+                        <Col md={3}><EmotionForm name={"Happiness"} numberClick={this.numberClick} /></Col>
+                        <Col md={3}><EmotionForm name={"Peacefulness"} numberClick={this.numberClick} /></Col>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <Col sm={3}><EmotionForm name={"Gratitude"} numberClick={this.numberClick} /></Col>
+                    </Row>
+                </Container>
             </div>
         )
     };
